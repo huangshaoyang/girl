44b4f3f5778e05f4dd0ee51669293dc707d1e441
augustAlpha <hsymail@163.com>
2018-10-31 11:06:02 +0800

    second commit

--- src/main/java/com/hsy/girl/GirlApplication.java
+++ src/main/java/com/hsy/girl/GirlApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GirlApplication {
 
     public static void main(String[] args) {
+        System.out.println("hello,world");
         SpringApplication.run(GirlApplication.class, args);
     }
 }
